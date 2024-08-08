f209fbbb3f174d998a60eee958025a347deb596d
Shashi Kiran <shashi.bolleddula@magicbricks.com>
2024-08-09 00:56:04 +0530

    Nav bar | Dashboard Cards Added

--- src/layout/NavigationBar.js
+++ src/layout/NavigationBar.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import profileImg from '../resources/static/images/profile.png'
+import '../resources/static/css/nav.css';
+
 
 const NavigationBar = () => {
   return (
-    <div>
-      <h3>NavigationBar</h3>
-    </div>
-  )
-}
+    <nav className="navbar">
+      <ul className="navbar__menu">
+        <li className="navbar__item">Home</li>
+        <li className="navbar__item">Transact</li>
+        <li className="navbar__item">Reports</li>
+        <li className="navbar__item">Latest Finance News</li>
+        <li className="">
+          <img src={profileImg} alt="Profile" className="navbar__profile-image"/>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import NavigationBar from '../layout/NavigationBar'
+import NavigationBar from '../layout/NavigationBar';
+import '../resources/static/css/dashboard.css';
 
 const Dashboard = () => {
   return (
     <>
+      <NavigationBar />
       <div className='container'>
-        <NavigationBar/>
-        <p>Welcome to Dashboard</p>
+        <section className='section'>
+          <div class="cards">
+            <a class="card income" href="/">
+              <p>Total Income</p>
+              <p class="small">Card description with lots of great facts and interesting details.</p>
+              <div class="go-corner" href="#">
+                <div class="go-arrow">→</div>
+              </div>
+            </a>
+            <a class="card expenses" href="/">
+              <p>Total Expenses</p>
+              <p class="small">Card description with lots of great facts and interesting details.</p>
+              <div class="go-corner" href="#">
+                <div class="go-arrow">→</div>
+              </div>
+            </a>
+            <a class="card savings" href="/">
+              <p>Total Savings</p>
+              <p class="small">Card description with lots of great facts and interesting details.</p>
+              <div class="go-corner" href="#">
+                <div class="go-arrow">→</div>
+              </div>
+            </a>
+            <a class="card status" href="/">
+              <p>Budget Status</p>
+              <p class="small">Card description with lots of great facts and interesting details.</p>
+              <div class="go-corner" href="#">
+                <div class="go-arrow">→</div>
+              </div>
+            </a>
+          </div>
+        </section>
       </div>
     </>
   );
